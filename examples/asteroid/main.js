dd0aa1b63badc6929544140bc9014cb92e75aef5
Ian Webster <ianw_git@ianww.com>
2018-12-12 21:52:15 -0800

    Add asteroid example

---
+++ examples/asteroid/main.js
@@ -0,0 +1,41 @@
+// Create the visualization and put it in our div.
+const viz = new Spacekit.Container(document.getElementById('main-container'), {
+  assetPath: '../../src/assets',
+  jed: 2458461.459,
+  debug: {
+    showAxesHelper: true,
+    showStats: true,
+  },
+});
+
+// Create a skybox using NASA TYCHO artwork.
+viz.createSkybox(Spacekit.SkyboxPresets.NASA_TYCHO);
+
+// Create our first object - the sun - using a preset space object.
+viz.createObject('sun', Spacekit.SpaceObjectPresets.SUN);
+
+// Then add some planets
+viz.createObject('mercury', Spacekit.SpaceObjectPresets.MERCURY);
+viz.createObject('venus', Spacekit.SpaceObjectPresets.VENUS);
+viz.createObject('earth', Spacekit.SpaceObjectPresets.EARTH);
+viz.createObject('mars', Spacekit.SpaceObjectPresets.MARS);
+viz.createObject('jupiter', Spacekit.SpaceObjectPresets.JUPITER);
+viz.createObject('saturn', Spacekit.SpaceObjectPresets.SATURN);
+viz.createObject('uranus', Spacekit.SpaceObjectPresets.URANUS);
+viz.createObject('neptune', Spacekit.SpaceObjectPresets.NEPTUNE);
+
+viz.createObject('aci', {
+  ephem: new Spacekit.Ephem({
+    epoch: 2458600.5,
+    a: 5.38533,
+    e: 0.19893,
+    i: 22.11137,
+    om: 294.42992,
+    w: 314.28890,
+    ma: 229.14238,
+  }, 'deg'),
+  ecliptic: {
+    displayLines: true,
+    lineColor: 0x333333,
+  },
+});

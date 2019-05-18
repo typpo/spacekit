cd70e821e498d89ffe77f772e743dab2258d6d95
Ian Webster <ianw_git@ianww.com>
2019-05-17 21:27:23 -0700

    create stars, not skybox in earth-moon example

--- examples/earth_moon/main.js
+++ examples/earth_moon/main.js
@@ -5,7 +5,8 @@ const viz = new Spacekit.Simulation(document.getElementById('main-container'), {
 });
 
 // Create a skybox using NASA TYCHO artwork.
-viz.createSkybox(Spacekit.SkyboxPresets.NASA_TYCHO);
+//viz.createSkybox(Spacekit.SkyboxPresets.NASA_TYCHO);
+viz.createStars();
 
 // Create our first object - the sun - using a preset space object.
 viz.createObject('sun', Spacekit.SpaceObjectPresets.SUN);

b1230ce0545573300a43e6238a282cead8db114c
Ian Webster <ianw_git@ianww.com>
2019-09-16 22:02:33 -0700

    It works!

--- build/spacekit.js
+++ build/spacekit.js
@@ -50754,6 +50754,12 @@ var Spacekit = (function (exports) {
 	    // TODO(ian): Accept either context or container
 	    this._context = context;
 
+	    this._camera = null;
+	    this._cameraControls = null;
+
+	    // Optional mesh that we are following.
+	    this._followMesh = null;
+
 	    this.init();
 	  }
 
@@ -50768,17 +50774,11 @@ var Spacekit = (function (exports) {
 	      rescaleNumber(2000),
 	    );
 
-	    // Workaround for using OrbitControls + camera that is not child of the
-	    // scene.  See
-	    // https://stackoverflow.com/questions/53292145/forcing-orbitcontrols-to-navigate-around-a-moving-object-almost-working
-	    // https://github.com/mrdoob/three.js/pull/16506
-	    //this._orbitControlsCamera = this._camera.clone();
-
 	    // Controls
 	    // TODO(ian): Set maxDistance to prevent camera farplane cutoff.
 	    // See https://discourse.threejs.org/t/camera-zoom-to-fit-object/936/6
-	    //const controls = new OrbitControls(this._orbitControlsCamera, this._simulationElt);
-	    //const controls = new OrbitControls(this._camera, this._simulationElt);
+	    const controls = new OrbitControls(this._camera, this._simulationElt);
+	    controls.enablePan = true;
 	    controls.zoomSpeed = 1.5;
 	    controls.userPanSpeed = 20;
 	    controls.rotateSpeed = 2;
@@ -50787,45 +50787,39 @@ var Spacekit = (function (exports) {
 	      TWO: TOUCH.DOLLY_ROTATE,
 	    };
 	    this._cameraControls = controls;
-
-	    // The mesh in which this camera is embedded.  If null, the camera floats
-	    // freely around the scene.
-	    this._cameraMesh = null;
 	  }
 
 	  /**
 	   * Move the camera so to follows a SpaceObject. Currently only works for
-	   * non-particlesystems.  @param {SpaceObject} obj SpaceObject to follow.
+	   * non-particlesystems.
+	   * @param {SpaceObject} obj SpaceObject to follow.
 	   * @param {Array.<Number>} position Position of the camera with respect to
 	   * the object.
 	   */
 	  followObject(obj, position) {
-	    // Attach camera to the object's mesh.
-	    // TODO(ian): Handle rotating object
-	    // https://stackoverflow.com/questions/12998137/camera-following-an-objects-rotation
-	    const cameraMesh = obj.get3jsObjects()[0];
-	    cameraMesh.add(this._camera);
+	    const followMesh = obj.get3jsObjects()[0];
 
-	    const newpos = cameraMesh.position;
-	    //this._cameraControls.target.set(newpos.x, newpos.y, newpos.z);
+	    this._cameraControls.enablePan = false;
 
 	    const rescaled = rescaleArray(position);
-	    this._camera.position.set(rescaled[0], rescaled[1], rescaled[2]);
-	    console.log('rescaled', rescaled);
+	    this._camera.position.add(
+	      new Vector3(rescaled[0], rescaled[1], rescaled[2]),
+	    );
 
 	    this._cameraControls.update();
-	    this._cameraMesh = cameraMesh;
+	    this._followMesh = followMesh;
 	  }
 
 	  stopFollowingObject() {
-	    if (this._cameraMesh) {
-	      this._cameraMesh.remove(this._camera);
-	      this._cameraMesh = null;
+	    if (this._followMesh) {
+	      this._followMesh.remove(this._camera);
+	      this._followMesh = null;
+	      this._cameraControls.enablePan = true;
 	    }
 	  }
 
 	  isFollowingObject() {
-	    return !!this._cameraMesh;
+	    return !!this._followMesh;
 	  }
 
 	  /**
@@ -50841,16 +50835,15 @@ var Spacekit = (function (exports) {
 
 	  update() {
 	    if (this.isFollowingObject()) {
-	      const newpos = this._cameraMesh.position.clone();
+	      const newpos = this._followMesh.position.clone();
+
+	      const offset = newpos.clone().sub(this._cameraControls.target);
+	      this._camera.position.add(offset);
 
 	      this._cameraControls.target.set(newpos.x, newpos.y, newpos.z);
 	    }
 
-	    console.log(this._camera.position);
-
-	    //this._camera.copy(this._orbitControlsCamera);
-
-	    // Handle trackball movements
+	    // Handle control movements
 	    this._cameraControls.update();
 	  }
 	}

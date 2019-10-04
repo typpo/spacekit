8509c9d886ecf0cd1b53f1fa1b90623d4afe94f5
Ian Webster <ianw_github@ianww.com>
2019-10-03 18:08:34 -0700

    Add autoreload and serve files directly from rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,7 @@
 import commonjs from 'rollup-plugin-commonjs';
+import livereload from 'rollup-plugin-livereload';
 import resolve from 'rollup-plugin-node-resolve';
+import serve from 'rollup-plugin-serve';
 
 export default {
   input: 'src/spacekit.js',
@@ -10,5 +12,10 @@ export default {
       file: 'build/spacekit.js',
     },
   ],
-  plugins: [resolve(), commonjs()],
+  plugins: [
+    resolve(),
+    commonjs(),
+    livereload(),
+    serve({ port: 8001, contentBase: ['.'] }),
+  ],
 };

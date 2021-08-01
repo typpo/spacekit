20c581046a96b9f8a98de78ea97ea27e568ea6b3
Ian Webster <ianw_github@ianww.com>
2021-08-01 09:56:41 -0700

    Use normal earth texture in example

--- examples/planet/main.js
+++ examples/planet/main.js
@@ -6,7 +6,7 @@ const viz = new Spacekit.Simulation(document.getElementById('main-container'), {
 viz.createStars();
 
 viz.createSphere('earth', {
-  textureUrl: './earth_66mya.jpg',
+  textureUrl: './eso_earth.jpg',
   debug: {
     showAxes: true,
   },

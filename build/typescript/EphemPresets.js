b7fbf923920cd90742d7b172f46441c2c3f52450
Ian Webster <ianw_github@ianww.com>
2026-04-04 21:21:39 -0700

    fix: use Earth-Moon GM for moon preset

--- build/typescript/EphemPresets.js
+++ build/typescript/EphemPresets.js
@@ -59,7 +59,7 @@ exports.EphemPresets = {
     }, 'deg', true /* locked */),
     MOON: new Ephem_1.Ephem({
         // https://nssdc.gsfc.nasa.gov/planetary/factsheet/moonfact.html
-        GM: 0.3986e15,
+        GM: Ephem_1.GM.EARTH_MOON,
         // Geocentric
         // https://ssd.jpl.nasa.gov/horizons.cgi#results
         epoch: 2458621.5,
